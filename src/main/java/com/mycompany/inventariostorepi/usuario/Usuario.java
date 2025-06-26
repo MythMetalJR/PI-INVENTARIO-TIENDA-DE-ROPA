5f6a734796793ab9b1208cff9c092d99606ffb14
MythMetal <wilsonrodri3000@gmail.com>
2025-06-26 11:53:18 -0500

    Inventario 1.1

--- src/main/java/com/mycompany/inventariostorepi/usuario/Usuario.java
+++ src/main/java/com/mycompany/inventariostorepi/usuario/Usuario.java
@@ -9,5 +9,54 @@ package com.mycompany.inventariostorepi.usuario;
  * @author Usuario
  */
 public class Usuario {
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private String rol;
+
+    public Usuario(String nombre, String email, String contraseña, String rol) {
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", email=" + email + ", contrase\u00f1a=" + contraseña + ", rol=" + rol + '}';
+    }
+    
     
 }
