0172c5f675ec0cbe937ffd2263aaf06576d4a821
xaviertoala <xaviertoala2006@gmail.com>
2025-06-26 11:29:45 -0500

    Inventario 1.0

---
+++ src/main/java/com/mycompany/inventariostorepi/InventarioStorePI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventariostorepi;
+
+/**
+ *
+ * @author Usuario
+ */
+public class InventarioStorePI {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+}
